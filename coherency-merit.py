54abb6d3021e59efbf99333a72c3948acfa0cf51
poorva-b <45121384+poorva-b@users.noreply.github.com>
2019-07-16 12:01:26 +0530

    Update coherency-merit.py

--- coherency-merit.py
+++ coherency-merit.py
@@ -3,19 +3,19 @@ import mysql
 
 #separate function because moment need not be newly calculated every time a field merit is calculated;
 #moment can be calculated once in the beginning of the night and have global access 
-def get_coherency_moment(field_db):
+def get_coherency_centroid(field_db):
     
     #from mysql database of fields, get fields that have OBS STATUS > 0
     
-    #calculate weighted moment of fields already observed as weight*field_coords, where
+    #calculate weighted centroid of fields already observed as weight*field_coords, where
     #weight = obs_status (in percentage) 
     for field in fields:
-        weighted_moment_ra += field.OBS_STATUS * field.RA
-        weighted_moment_dec += field.OBS_STATUS * field.DEC
+        weighted_centroid_ra += field.OBS_STATUS * field.RA
+        weighted_centroid_dec += field.OBS_STATUS * field.DEC
     
-    moment = (weighted_moment_ra/len(fields), weighted_moment_dec/len(fields))
+    centroid = (weighted_centroid_ra/len(fields), weighted_centroid_dec/len(fields))
     
-    return moment #return a tuple of (ra, dec)
+    return centroid #return a tuple of (ra, dec)
 
 def angular_dist(ra1,dec1,ra2,dec2):
       r1=np.radians(ra1)
@@ -29,6 +29,6 @@ def angular_dist(ra1,dec1,ra2,dec2):
       return d
 
 def get_coherency_merit(self):
-    d = angular_dist(self.ra, self.dec, moment[0], moment[1])
+    d = angular_dist(self.ra, self.dec, centroid[0], centroid[1])
     return 1/d
     
